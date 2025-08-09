03b4dbbaaf34f753a6f6e062e3fa0206a3229d76
Aloamoh3 <aloamoh@icloud.com>
2025-08-09 18:18:25 -0400

    Create main.dart

---
+++ lib/widgets/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:on_track/controllers/view_controller.dart';
+import 'package:on_track/widgets/router.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late List<ThemeData> _themes;
+  final ViewController _viewController = Get.put(ViewController());
+
+  @override
+  void initState() {
+    super.initState();
+    _generateThemes();
+  }
+
+  void _generateThemes() {
+    final themes = <ThemeData>[];
+    for (final brightness in [Brightness.light, Brightness.dark]) {
+      themes.add(
+        ThemeData(
+          brightness: brightness,
+          colorSchemeSeed: const Color(0xFFFD581C),
+          textTheme: GoogleFonts.geologicaTextTheme(
+            const TextTheme(
+              headlineLarge: TextStyle(fontWeight: FontWeight.w600),
+            ),
+          ),
+        ),
+      );
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.init(context, designSize: const Size(390, 844));
+    return Obx( () {
+      final brightness = _viewController.state.value;
+      final themeMode = switch (brightness) {
+        Brightness.light => ThemeMode.light,
+        Brightness.dark => ThemeMode.dark,
+      };
+       return GetMaterialApp(
+        title: "On Track",
+        debugShowCheckedModeBanner: false,
+        themeMode: themeMode,
+        theme: _themes[0],
+        getPages: appScreen,
+      );
+   });
+  }
+ }
